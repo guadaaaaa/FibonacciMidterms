96f269317d9c77242663924629f9ca86d3f5c413
guadaaaaa <obandoguadalue951@gmail.com>
2024-03-19 18:04:57 +0800

    Kapoy nako

--- src/FibRunnable.java
+++ src/FibRunnable.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class FibRunnable {
+class FibRunnable implements Runnable {
+    private int n;
+    public static int[] result;
+
+    public FibRunnable(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public void run() {
+        if(n==1){
+            result[0] = 0;
+            System.out.println("Fibonacci Number 1 : 0");
+            return;
+        }else if(n==2){
+            result[1] = 1;
+            System.out.println("Fibonacci Number 2 : 1");
+            return;
+        }
+        Thread t1 = Main.threadArrayList.get(n-3);
+        Thread t2 = Main.threadArrayList.get(n-2);
+        try {
+            t1.start();
+            t2.start();
+        } catch (IllegalThreadStateException e) {
+        }
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+        int res = result[n-2] + result[n-3];
+        result[n-1] = res;
+        System.out.println("Fibonacci Number " + n + " : " + res);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static ArrayList<Thread> threadArrayList;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        threadArrayList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of Fibonacci Terms: ");
+        int num = sc.nextInt();
+        FibRunnable.result = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            threadArrayList.add(new Thread(new FibRunnable(i+1)));
+        }
+
+        Thread lastThrd = threadArrayList.get(threadArrayList.size() - 1);
+        lastThrd.start();
+
+        try {
+            lastThrd.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+
+        System.out.println("\nThe " + num + " Fibonacci numbers:");
+        for (int i = 0; i < num; i++) {
+            System.out.print(FibRunnable.result[i] + " ");
+        }
     }
 }
